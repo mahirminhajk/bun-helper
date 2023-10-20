2d9e2d375aa4c46edb55fb404ffa7517addede97
mahirminhajk <kmahirminhaj@gmail.com>
2023-10-20 23:11:46 +0530

    project initialized

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/", () => "Hello Elysia")
+  .get("/hello", () => "Hello World")
+  .get('/product/:id', ({params: {id}})=> `Product ${id}`)
+  .listen(5000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
